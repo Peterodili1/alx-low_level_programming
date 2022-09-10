1711658ff1012ab73f775787330e45ddc34ee95d
peterodili1 <peterodili604@gmail.com>
2022-09-10 14:44:05 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
+
 	{
 		printf("greater than 5\n");
 	}
+
 	else if (n % 10 == 0)
+
 	{
-			printf("0\n");
+	
+		 printf("0\n");
 	}
+
 	else
 		
 	{
