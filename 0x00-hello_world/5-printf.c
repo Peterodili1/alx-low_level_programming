c810c7145d229007c037500c6c32f04d41852ecc
peterodili1 <peterodili604@gmail.com>
2022-09-10 06:35:31 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#incliude <stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Return:0 if no erros, return non zero if erros
+ */
+int main (void)
+{
+	printf("with proper programmar, but  the outcome is a piece of art,\n");
+	return (0);
+}
