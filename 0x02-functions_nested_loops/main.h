84baf0a46b1a922eed8e9d0d9b31dc0ec065a383
peterodili1 <peterodili604@gmail.com>
2022-09-14 18:35:21 -0700

    tas 12

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
-#ifndef HOLBERTON_H_CONCLUDED
-#define HOLBERTON_H_CONCLUDED
-
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabnet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,8 +8,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
-void print_times_table(int r, int n);
-
-#endif
+void print_times_table(int n);
