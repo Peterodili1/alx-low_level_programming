021a44e40a15b07379271116aaf61f34ba43c7b5
peterodili1 <peterodili604@gmail.com>
2022-09-16 00:42:34 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -11,6 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
+	
 	return (0);
 }
