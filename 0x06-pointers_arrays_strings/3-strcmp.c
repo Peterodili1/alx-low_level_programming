ba3b0e8caf7663b5c0849fbf5e1e51207947c768
peterodili1 <peterodili604@gmail.com>
2022-09-21 16:01:00 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- *  _strcmp -> for comparing purpose
- *  @s1: param1
- *  @s2: param2
- *   Return: integer
+ * _strcmp -> for comparing purpose
+ * @s1: param1
+ * @s2: param2
+ * Return: integer
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,5 +13,5 @@ int _strcmp(char *s1, char *s2)
 	result = strcmp(s1, s2);
 	return (result);
 }
-footer
+Footer
 
