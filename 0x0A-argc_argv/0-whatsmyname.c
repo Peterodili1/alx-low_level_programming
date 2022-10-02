30c2615e2e8f8ebcc1480b00406a680dcb7b275a
peterodili1 <peterodili604@gmail.com>
2022-10-02 03:04:11 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,13 +7,13 @@
  * you should not remove the path before the name of the program
  *
  * @argc: This is the argument count
- * @argc: This is the argument vector
+ * @argv: This is the argument vector
  *
  * Return: this return to 0
  */
-int main(int argc, char *argc[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argc[]);
+	printf("%s\n", argv[0]);
 	(void)argc;
 	return (0);
 }
