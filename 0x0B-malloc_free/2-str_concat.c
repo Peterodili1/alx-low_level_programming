86516b37730ec51e47ded610c8aaf85fd8aa8cc9
peterodili1 <peterodili604@gmail.com>
2022-10-03 12:04:51 -0700

    task crrection

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,7 @@
 
 /**
 *str_concat - concatenates 2 strings.
-*a NULL string is treated as an empty string
+*a NULL string is treated as an empty string.
 *@s1: pointer to string.
 *@s2: pointer to string.
 *Return: pointer to newly allocated memory which
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int len1, len2, size, i, j;
 	char *nstr;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -25,14 +25,13 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
-
 	len2 = 0;
-	while (s1[len1] != '\0')
-		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
 	size = len1 + len2;
-	nstr = malloc((sizeof(char) * size) + 1);
 
+	nstr = malloc((sizeof(char) * size) + 1);
 	/*check if malloc was successful */
 	if (nstr == NULL)
 		return (NULL);
