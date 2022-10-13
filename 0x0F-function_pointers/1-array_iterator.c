432c19cb664c3a1dbaccf46926d80999fe27e0c1
peterodili1 <peterodili604@gmail.com>
2022-10-12 21:05:24 -0700

    correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,11 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
 /**
 *array_iterator - iterates an array
 *@array: is an array
 *@size: is the size
 *@action: is something
 */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
